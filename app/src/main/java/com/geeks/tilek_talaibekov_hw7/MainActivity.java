407daec08538905ce9bfa3445a4c6d9413ec76ac
asant <asantursunaliev0606@gmail.com>
2024-05-02 14:00:03 +0600

    а

--- app/src/main/java/com/geeks/tilek_talaibekov_hw7/MainActivity.java
+++ app/src/main/java/com/geeks/tilek_talaibekov_hw7/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Integer firstOperand, secondOperand, result;
     private String operation;
-    private Boolean isOperationClick;
+    private boolean isOperationClick;
 
     public static final String KEY1 = "key1";
 
@@ -36,11 +36,8 @@ public class MainActivity extends AppCompatActivity {
             secondOperand = 0;
         } else if (view instanceof MaterialButton) {
             String text = ((MaterialButton) view).getText().toString();
-            if (textView.getText().toString().equals("0") || isOperationClick) {
-                textView.setText(text);
-            } else {
-                textView.append(text);
-            }
+            if (textView.getText().toString().equals("0") || isOperationClick) textView.setText(text);
+            else textView.append(text);
         }
         isOperationClick = false;
     }
@@ -48,49 +45,47 @@ public class MainActivity extends AppCompatActivity {
     public void onOperationClick(View view) {
         Button invisibleButton = findViewById(R.id.invisible_button);
         invisibleButton.setVisibility(View.INVISIBLE);
-        if (view.getId() == R.id.btn_plus) {
+        if (isOperationClick) return;
+
+        int viewId = view.getId();
+        if (viewId == R.id.btn_plus || viewId == R.id.btn_minus || viewId == R.id.btn_multiplication || viewId == R.id.btn_division) {
             firstOperand = Integer.valueOf(textView.getText().toString());
             isOperationClick = true;
-            operation = "+";
-        } else if (view.getId() == R.id.btn_minus) {
-            firstOperand = Integer.valueOf(textView.getText().toString());
-            isOperationClick = true;
-            operation = "-";
-        } else if (view.getId() == R.id.btn_multiplication) {
-            firstOperand = Integer.valueOf(textView.getText().toString());
-            isOperationClick = true;
-            operation = "x";
-        } else if (view.getId() == R.id.btn_division) {
-            firstOperand = Integer.valueOf(textView.getText().toString());
-            isOperationClick = true;
-            operation = "/";
-        } else if (view.getId() == R.id.btn_equal) {
+            operation = ((Button) view).getText().toString();
+        } else if (viewId == R.id.btn_equal) {
             secondOperand = Integer.valueOf(textView.getText().toString());
             invisibleButton.setVisibility(View.VISIBLE);
-            if (operation.equals("+")) {
-                result = firstOperand + secondOperand;
-            } else if (operation.equals("-")) {
-                result = firstOperand - secondOperand;
-            } else if (operation.equals("x")) {
-                result = firstOperand * secondOperand;
-            } else if (operation.equals("/")) {
-                if (secondOperand != 0) {
-                    result = firstOperand / secondOperand;
-                } else {
-                    Toast.makeText(this, "Ошибка. Деление на ноль нельзя.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-            textView.setText(result.toString());
+            calculateResult();
         }
-        isOperationClick = true;
     }
 
     public void onButtonClick(View view) {
-        if (view.getId() == R.id.invisible_button){
+        if (view.getId() == R.id.invisible_button) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            intent.putExtra(MainActivity.KEY1, textView.getText().toString());
+            intent.putExtra(KEY1, textView.getText().toString());
             startActivity(intent);
         }
+    }
+
+    private void calculateResult() {
+        switch (operation) {
+            case "+":
+                result = firstOperand + secondOperand;
+                break;
+            case "-":
+                result = firstOperand - secondOperand;
+                break;
+            case "x":
+                result = firstOperand * secondOperand;
+                break;
+            case "/":
+                if (secondOperand != 0) result = firstOperand / secondOperand;
+                else {
+                    Toast.makeText(this, "Ошибка. Деление на ноль нельзя.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                break;
+        }
+        textView.setText(result.toString());
     }
 }
